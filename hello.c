188146539f381b38c436f19d07e9f0ac47f7f874
Ethan Steele <esteele6@huskers.unl.edu>
2024-08-29 11:38:47 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ethan Steele
+ *         esteele6@huskers.unl.edu
+ * Date: 2024/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Ethan Steele!\n");
+  printf("I am a Computer Science major!\n");
+
+  return 0;
+}
